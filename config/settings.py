8d6cf8c98473473b3a615ebab628dfa515bedecf
Juraqulov Azizbek <sen@gmail.com>
2026-03-09 14:24:38 +0500

    static

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 if DEBUG:
     STATICFILES_DIRS = [BASE_DIR / 'static']
 else:
-    STATIC_ROOT = '/var/www/django-deploy/static/'
+    STATIC_ROOT = '/var/www/My-Web-Server/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
